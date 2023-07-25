39be499944452801bf30dae662cc50d965cb1f98
X474X <paultudorstefan@gmail.com>
2023-07-25 16:15:25 +0300

    Finished project

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const inputName = document.querySelector("input");
 const search = document.querySelector("button");
 const title = document.querySelector("h3");
+const year = document.querySelector(".year");
+const rating = document.querySelector(".rating");
+const descriere = document.querySelector("p");
+const poster = document.querySelector("img");
 
 const getData = async () => {
   const response = await fetch(
@@ -9,8 +13,14 @@ const getData = async () => {
   const movie = await response.json();
   console.log(movie);
   title.innerHTML = movie.Title;
+  year.innerHTML = movie.Year;
+  rating.innerHTML = movie.imdbRating + "/10";
+  descriere.innerHTML = movie.Plot;
+  poster.src = movie.Poster;
 };
 
 search.addEventListener("click", () => {
   getData();
 });
+
+console.log(poster);
